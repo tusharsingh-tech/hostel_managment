a57c3425d11f0aeabd9c51154a2090b15d87b811
Tushar Singh <tusharsinghqqqq@gmail.com>
2026-03-28 16:18:25 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,16 @@
 backend/login.php
+<?php
+$conn = mysqli_connect("localhost","root","","hostel");
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$result = mysqli_query($conn,
+"SELECT * FROM users WHERE email='$email' AND password='$password'");
+
+if(mysqli_num_rows($result)>0){
+    header("Location: ../dashboard.php");
+}else{
+    echo "Login Failed";
+}
+?>
