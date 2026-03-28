8ff40beb7f13c11a69ee3f69e58136dac419d3e1
Tushar Singh <tusharsinghqqqq@gmail.com>
2026-03-28 16:13:02 +0000

    Create register.php

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect("localhost","root","","hostel");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+mysqli_query($conn,"INSERT INTO users(name,email,password)
+VALUES('$name','$email','$password')");
+
+echo "Registered!";
+?>
